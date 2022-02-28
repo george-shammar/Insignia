a1df9b238302618a35bbc49b19d7732c8844d1f0
george-shammar <george.gbenle@outlook.com>
2022-02-28 11:02:36 +0000

    Update nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Whitepaper from './Whitepaper';
-// import logo from "../assets/logo.png";
+import logo from "../assets/Logo.png";
 import "../styles/Nav.css"
 
 function Nav() {
@@ -9,16 +9,17 @@ function Nav() {
       <div className="top-nav">
       <div className="d-flex justify-content-between align-text-center pt-2 px-4">
         <div className="azania d-flex">
-          <Link to="/" className="pink">Insignia</Link>
-          {/* <Link to="/" ><img alt="logo" src={logo} className="logo mx-2 my-2" /></Link> */}
+          {/* <Link to="/" className="pink">Insignia</Link> */}
+          <Link to="/" ><img alt="logo" src={logo} className="logo" /></Link>
         </div>
         <div className="d-flex align-items-center font-meridian">
           {/* <p> <Link to="/CreateItem" className="white">Create</Link></p>
           <p> <Link to="/MarketPLace" className="px-4 white">Market Place</Link></p>
           <p><Link to="/MyAssets" className="white">My Assets</Link></p>
           <p><Link to="/Dashboard" className="px-4 white">Dashboard</Link></p> */}
+          <p> <Link to="/" className="px-4 white">Home</Link></p>
           <p><Link to="/Whitepaper" className="px-4 white">Whitepaper</Link></p>
-        <p className="px-3 border-0 rounded-pill p-2 wallet white">Connect Wallet</p>
+        <p className="px-3 border-0 rounded-pill p-2 wallet white">Play Now</p>
         </div>
       </div><hr className="hr"/>
       </div>

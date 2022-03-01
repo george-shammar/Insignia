85069075ed4c867021dece0443ced3d97215e7ab
george-shammar <george.gbenle@outlook.com>
2022-03-01 02:28:15 +0000

    Add image

---
+++ src/components/Trailer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import header from '../assets/header1.png';
+
+const Trailer = () => (
+  <div>
+      <div>
+        <img src={header}/>
+        <p>Trailer</p>
+      </div>
+     
+  </div>
+  
+);
+
+export default Trailer;

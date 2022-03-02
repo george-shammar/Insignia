aa96f7ff5e4e4e6ae70bf170879640b580c817be
george-shammar <george.gbenle@outlook.com>
2022-03-02 05:43:33 +0000

    Create Play now

--- src/components/Join.js
+++ src/components/Join.js
@@ -6,7 +6,18 @@ const Join = () => {
   return(
     <div className="join">
       <h6 className="topic gold font-medieval py-5">Join Us</h6>
-      <img src={join} className="join-image"/>
+      <p className="white join-text font-meridian">Join the conversation on our social media channels.</p>
+      <div className="join-social">
+        <div className="d-flex fav-icons center">
+          <p><a  href="#"><i className="fab fa-discord white"></i></a></p>
+          <p><a href="#"><i className="fab fa-twitter white"></i></a></p>
+          <p><a href="#"><i className="fab fa-telegram white"></i></a></p>
+        </div>
+        <img src={join} className="join-image"/>
+      </div>
+      
+      
+      
 
     </div>
   )

f6b55d01e248594c68ffe26f1a172b307743fc04
george-shammar <george.gbenle@outlook.com>
2022-03-02 02:11:20 +0000

    Create event

--- src/components/Community.js
+++ src/components/Community.js
@@ -1,10 +1,15 @@
-
+import '../styles/Community.css'
+import header from '../assets/bottom_brown.png';
 const Community = () => {
     return (
         <div>
-             
-        </div>
+             <img src={header} className="rough"/>
         
+            <div className="community">
+                
+            </div>
+        
+        </div>
     
     )
 }

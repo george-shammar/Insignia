a0e84ffc954d9dcabdf5857d40c9ec272bf233c9
george-shammar <george.gbenle@outlook.com>
2022-03-02 04:20:26 +0000

    Update Home

--- src/components/Trailer.js
+++ src/components/Trailer.js
@@ -12,9 +12,13 @@ const Trailer = () => {
     <div className="trailer">
       <h6 className="topic font-medieval py-5">Watch The Trailer</h6>
       <p className="gold font-meridian sub">Use a headset for maximum effect</p>
-      <div className="px-3 video-responsive">
+      
+            <div className="px-3 video-responsive">
                 <iframe className="youtube" src="https://www.youtube.com/embed/gSLiX1QihEc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
+          
+      
+            
     </div>
 
     <img src={footer} className="rough"/>

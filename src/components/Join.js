b712f7d7bf82fefbc6008a67600296adaa2462f4
george-shammar <george.gbenle@outlook.com>
2022-03-02 04:53:12 +0000

    Update Home

--- src/components/Join.js
+++ src/components/Join.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../styles/Join.css';
+import join from '../assets/join.png';
 
 const Join = () => {
   return(
     <div className="join">
-      
+      <h6 className="topic gold font-medieval py-5">Join Us</h6>
+      <img src={join} className="join-image"/>
 
     </div>
   )

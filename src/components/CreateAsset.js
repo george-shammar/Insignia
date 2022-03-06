4683e44ff14d18bb41bdc07c8eb05770ed78019f
george-shammar <george.gbenle@outlook.com>
2022-03-06 21:13:14 +0000

    Implement copyright

--- src/components/CreateAsset.js
+++ src/components/CreateAsset.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Nav from "./Nav";
+import Copyright from './Copyright';
 
 const CreateAsset = () => (
-  <div className='play'>
-      <div>
-      <Nav />
-        <p>Create Asset</p>
+  <div>
+      <div className='play'>
+          <div>
+          <Nav />
+            <p>Create Asset</p>
+          </div>
+        
       </div>
-     
+        <Copyright />
   </div>
-  
 );
 
 export default CreateAsset;

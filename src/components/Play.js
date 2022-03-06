af76a91591f03ebbd77fb576e38c3e95c47777ad
george-shammar <george.gbenle@outlook.com>
2022-03-06 12:44:48 +0000

    Create play page

--- src/components/Play.js
+++ src/components/Play.js
@@ -6,27 +6,35 @@ import '../styles/Play.css';
 const Play = () => (
   <div className="play">
     <Nav />
-      <div className='d-flex steps pt-5'>
+      <div className='d-flex steps px-5 pt-5'>
 
-        <div className='child'>
-          <p className='step-figure py-2 rounded center'>1</p>
-          <p>NEAR Wallet Set up</p>
-        </div>
+          <div className='child'>
+            <p className='step-figure py-2 rounded center'>1</p>
+            {/* <p className='line mt-2'>----------------------------------</p> */}
+            <p>NEAR Wallet Set up</p>
+          </div>
+          
+
         
-        <div>
-          <p>2</p>
-          <p>Get Insignia Token</p>
-        </div>
+          <div className='child'>
+            <p className='step-figure py-2 rounded center'>2</p>
+            {/* <p className='line mt-2'>----------------------------------</p> */}
+            <p>Get Insignia Token</p>
+          </div>
         
-        <div>
-          <p>3</p>
-          <p>Creat Game Asset</p>
-        </div>
+
+          <div className='child'>
+            <p className='step-figure py-2 rounded center'>3</p>
+            {/* <p className='line mt-2'>----------------------------------</p> */}
+            <p>Creat Game Asset</p>
+          </div>
         
-        <div>
-          <p>4</p>
-          <p>Download & Play</p>
-        </div>
+          
+          <div className='child'>
+            <p className='step-figure py-2 rounded center'>4</p>
+            <p>Download & Play</p> 
+          </div>
+        
         
         
       </div>

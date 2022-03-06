d7d634515d9b88feb320a7a0bb2a71094a09eb40
george-shammar <george.gbenle@outlook.com>
2022-03-06 13:13:47 +0000

    Create play page

--- src/components/Play.js
+++ src/components/Play.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Nav from './Nav';
+import WalletSetup from './WalletSetup';
+import { Link } from 'react-router-dom';
 import '../styles/Play.css';
 
 
@@ -8,15 +10,19 @@ const Play = () => (
     <Nav />
       <div className='d-flex steps px-5 pt-5'>
 
-          <div className='child'>
+      <Link to="/WalletSetup" className='play-text'><div className='child'>
+        
             <p className='step-figure py-2 rounded'>1</p>
             {/* <p className='line mt-2'>----------------------------------</p> */}
             <p className='mt-3'>NEAR Wallet Set up</p>
-          </div>
+        
+      </div>
+      </Link>
           
 
         
           <div className='child'>
+
             <p className='step-figure py-2 rounded'>2</p>
             {/* <p className='line mt-2'>----------------------------------</p> */}
             <p className='mt-3'>Get Insignia Token</p>
---
+++ src/components/WalletSetup.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Nav from "./Nav";
+
+const WalletSetup = () => (
+  <div className='play'>
+      <div>
+      <Nav />
+        <p>Set up wallet</p>
+      </div>
+     
+  </div>
+  
+);
+
+export default WalletSetup;

0824e56a015d482f92fc06eff6fb5b67142556bf
george-shammar <george.gbenle@outlook.com>
2022-03-06 11:54:04 +0000

    Create play page

--- src/components/Play.js
+++ src/components/Play.js
@@ -5,14 +5,27 @@ import '../styles/Play.css';
 const Play = () => (
   <div className="play">
       <div>
-        <p>1</p>
-        <p>NEAR Wallet Set up</p>
-        <p>2</p>
-        <p>Get Insignia Token</p>
-        <p>3</p>
-        <p>Creat Game Asset</p>
-        <p>4</p>
-        <p>Download & Play</p>
+        
+        <div>
+          <p>1</p>
+          <p>NEAR Wallet Set up</p>
+        </div>
+        
+        <div>
+          <p>2</p>
+          <p>Get Insignia Token</p>
+        </div>
+        
+        <div>
+          <p>3</p>
+          <p>Creat Game Asset</p>
+        </div>
+        
+        <div>
+          <p>4</p>
+          <p>Download & Play</p>
+        </div>
+        
         
       </div>
      

7733ba18e67050eca076433ef15b545c8211b6a4
george-shammar <george.gbenle@outlook.com>
2022-03-07 01:02:39 +0000

    Implement ProgresbAR

--- src/components/WalletSetup.js
+++ src/components/WalletSetup.js
@@ -5,6 +5,7 @@ import ProgressBar from './ProgressBar';
 import { Link } from 'react-router-dom';
 import forwardArrow from '../assets/right-arrows.png';
 import backwardArrow from '../assets/left-arrow.png';
+import near from '../assets/near-logo.png';
 import '../styles/WalletSetup.css';
 
 const WalletSetup = () => (
@@ -26,10 +27,16 @@ const WalletSetup = () => (
         <div className="border wallet-space py-5 mt-5 shadow-lg">
             <article className="px-2">
                         <h6 className="bold pb-3 action font-meridian">WALLET SETUP</h6>
-                        <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        <p className="font-meridian">
+                          Feel free to skip this section if you already have a NEAR account. Otherwise, follow the simple instructions in the link below to get setup.
                         </p>
             </article>
+
+            <img src={near} className="near-logo"/>
+
+            <div>
+                    <a href="https://wallet.near.org/" target="_blank" className="wallet-scroll white font-meridian rounded-pill mb-0 mt-5 py-2 px-5">Get NEAR Wallet</a>
+            </div>
         </div>
 
 

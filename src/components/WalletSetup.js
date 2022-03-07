7f653c49df93c994417423c8debedd794abe2f46
george-shammar <george.gbenle@outlook.com>
2022-03-07 00:16:05 +0000

    Implement ProgresbAR

--- src/components/WalletSetup.js
+++ src/components/WalletSetup.js
@@ -17,9 +17,34 @@ const WalletSetup = () => (
           <ProgressBar />
         </div>
         <div className='font-meridian near-wallet pt-5'>
-        <h3>Create your NEAR wallet</h3>
+          <h3>Create your NEAR wallet</h3>
           <p>Insignia lives on the NEAR protocol. NEAR Wallet is your ticket to our metaverse.</p>
         </div>
+
+
+
+        <div className="border rounded card-space shadow-lg">
+            <article className="px-2">
+                        {/* <i className="fas fa-door-open pink py-4 icon-size action"></i> */}
+                        <h6 className="bold pb-3 action font-meridian">WALLET SETUP</h6>
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+            </article>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
         
           <div className='d-flex setup-navigation pt-5 mb-5'>
               <Link to="/" className='font-meridian back'><p><img src={backwardArrow} className="arrow"/> Back</p></Link>

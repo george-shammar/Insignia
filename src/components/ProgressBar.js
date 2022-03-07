e4f04c2ac387fbe77b8fe5f1c035e965b268089b
george-shammar <george.gbenle@outlook.com>
2022-03-07 00:02:39 +0000

    Implement ProgresbAR

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -7,33 +7,33 @@ const ProgressBar = () => (
     <div className='d-flex steps px-5 pt-5 font-meridian'>
         <Link to="/WalletSetup" className='child'>
           <div >
-              
               <p className='step-figure one py-2 rounded'>1</p>
-              {/* <p className='line mt-2'>----------------------------------</p> */}
+              
               <p className='mt-3'>NEAR Wallet Set up</p>
           
           </div>
         </Link>
             
-
+        <p className='line'>__________</p>
           
         <Link to="/GetToken" className='child'>
           <div>
 
               <p className='step-figure two py-2 rounded'>2</p>
-              {/* <p className='line mt-2'>----------------------------------</p> */}
               <p className='mt-3'>Get Insignia Token</p>
             </div>
         </Link>
           
+        <p className='line'>__________</p>
 
         <Link to="/CreateAsset" className='child'> <div>
               <p className='step-figure py-2 rounded'>3</p>
-              {/* <p className='line mt-2'>----------------------------------</p> */}
               <p className='mt-3'>Creat Game Asset</p>
             </div>
         </Link>
-            
+
+        <p className='line'>__________</p>
+
         <Link to="/DownloadGame" className='child'> 
             <div>
               <p className='step-figure py-2 rounded'>4</p>

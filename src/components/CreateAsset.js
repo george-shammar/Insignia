39ef71ab7c3a8d59ddc67d83c608fc11b63516af
george-shammer <george.gbenle@outlook.com>
2022-03-19 11:08:06 +0000

    Create config

--- src/components/CreateAsset.js
+++ src/components/CreateAsset.js
@@ -5,7 +5,7 @@ import Copyright from './Copyright';
 import { Link } from 'react-router-dom';
 import forwardArrow from '../assets/right-arrows.png';
 import backwardArrow from '../assets/left-arrow.png';
-import { initContract, login, logout } from '../utils';
+import { login, logout } from '../utils';
 // import { login, logout } from "./utils";
 import getConfig from "../config";
 

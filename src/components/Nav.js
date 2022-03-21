c13e028520d93272c00e1a1625f6dbf09a926b3c
george-shammar <george.gbenle@outlook.com>
2022-03-21 18:18:25 +0000

    Update Event component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -13,8 +13,8 @@ function Nav() {
         <div className="d-flex align-items-center font-meridian flex2">
           <p> <Link to="/" className="px-4 white">Home</Link></p>
           <p><a href="https://getinsignia.gitbook.io/whitepaper/" target="_blank" className="px-4 white">Whitepaper</a></p>
-          <p> <Link to="/MarketPLace" className="px-4 white">Market Place</Link></p>
-          <p> <Link to="/News" className="px-4 white">News</Link></p>
+          {/* <p> <Link to="/MarketPLace" className="px-4 white">Market Place</Link></p> */}
+          {/* <p> <Link to="/News" className="px-4 white">News</Link></p> */}
         
         </div>
         <div className="flex1">

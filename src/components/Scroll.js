bcb3c6c89f7dcb1a37a7140f4da0189d49a08093
george-shammar <george.gbenle@outlook.com>
2022-03-21 18:13:53 +0000

    Update Scroll component

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -15,8 +15,8 @@ const Scroll = () => {
             <p className='get-started font-roboto'>Click "Play Now" to follow the steps below and get started with Insignia</p>
             <div className="d-flex py-5 px-5 justify-content-between font-roboto">
             <div className="border rounded card-space shadow-lg">
-                    <article className="px-2">
-                        <i className="fas fa-door-open pink py-4 icon-size action"></i>
+                    <article className="px-2 pt-4">
+                       
                         <h6 className="bold pb-3 action font-meridian">WALLET SETUP</h6>
                         <p>
                         Insignia lives on the NEAR protocol. NEAR Wallet is your ticket to our metaverse. For first time NEAR users, click "Get NEAR Wallet", otherwise, proceed to the next tab.
@@ -24,41 +24,32 @@ const Scroll = () => {
                     </article>
                 </div>
                 <div className="border rounded card-space shadow-lg">
-                <article className="px-2">
-                    <i class="fas fa-shopping-cart py-4 icon-size action"></i>
+                <article className="px-2 pt-4">
+                    
                     <h6 className="bold pb-3 action font-meridian">GET TOKEN</h6>
                     <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Insignia Token is the native currency of our metaverse. Details about the tokenomics can be found in the Whitepaper. We are currently using NEAR as the means of exchange. Click "Fund Wallet" and visit any of the exchanges in the list to fund your wallet. To do anything with the NEAR Wallet, you will need at least 0.1 NEAR. However, on the testnet, NEAR wallet is pre-funded when you created an account.
                     </p>
                 </article>
                 </div>
                 <div className="border rounded card-space shadow-lg">
-                <article className="px-2">
-                <i class="fas fa-paint-brush pink py-4 icon-size action"></i>
+                <article className="px-2 pt-4">
+                
                     <h6 className="bold pb-3 action font-meridian">CREATE GAME ASSET</h6>
                     <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Each game asset is a unique NFT stored on IPFS. Each wallet can only have one Insignia NFT. Click "Connect Wallet" to link your NEAR Wallet. 
+                    Click "Create Asset" to mint a unique NFT. An alternative is to purchase a Game Asset in the Marketplace.
+
                     </p>
                 </article>
                 </div>
-                
-                {/* <div className="border rounded card-space shadow-lg">
-                <article className="px-2">
-                    <i class="fas fa-book-open py-4 icon-size action"></i>
-                    <h6 cclassName="bold pb-3 action font-meridian">DOWNLOAD AND PLAY</h6>
-                    <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    </p>
-               
-                </article>
-                </div> */}
 
                 <div className="border rounded card-space shadow-lg">
-                <article className="px-2">
-                    <i class="fas fa-shopping-cart py-4 icon-size action"></i>
+                <article className="px-2 pt-4">
                     <h6 className="bold pb-3 action font-meridian">DOWNLOAD & PLAY</h6>
                     <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Currently, a very limited edition of the Free-To-Play model of the game is available on PC. Android and IOS release is coming soon.
+
                     </p>
                 </article>
                 </div>

8f13383ee16231a886992c85c32a0108fa9cd0d6
george-shammar <george.gbenle@outlook.com>
2022-03-21 19:23:43 +0000

    Update socials

--- src/components/Join.js
+++ src/components/Join.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import discord from '../assets/discord-icon.png';
+import twitter from '../assets/twitter.png';
 import '../styles/Join.css';
 import join from '../assets/join.png';
 
@@ -7,14 +9,13 @@ const Join = () => {
     <div className="join">
       <h6 className="white topic-join font-medieval py-5">Join Us</h6>
       <p className="white join-text font-meridian">Join the conversation on our social media channels.</p>
-      <div className="join-social">
-        <div className="d-flex fav-icons center">
-          <p><a  href="#"><i className="fab fa-discord white"></i></a></p>
-          <p><a href="#"><i className="fab fa-twitter white"></i></a></p>
-          <p><a href="#"><i className="fab fa-telegram white"></i></a></p>
-        </div>
+      
+            <div className='soc d-flex'>
+                <a href="https://discord.gg/A4xfZENW2Z" target="_blank"><img src={discord} className=' join-soc'/></a>
+                <a href="https://twitter.com/Insignia_verse?s=03" target="_blank"><img src={twitter} className='join-soc'/></a>
+            </div>
         <img src={join} className="join-image"/>
-      </div>
+      
       
       
       

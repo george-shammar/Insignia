314038f9c0eeeeef6013f897369aeee47abd2ae7
george-shammer <george.gbenle@outlook.com>
2022-03-26 10:54:11 +0000

    Update marketplace

--- src/components/Marketplace.js
+++ src/components/Marketplace.js
@@ -1,10 +1,12 @@
 import Nav from "./Nav";
+import Copyright from "./Copyright";
 
 function Marketplace() {
     return (
-        <div className="play">
+        <div className="marketplace">
             <Nav />
             <h1>Marketplace</h1>
+            <Copyright />
         </div>
     )
 }

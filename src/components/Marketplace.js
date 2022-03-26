5b730ba08f453fc0e3a8d2975706033df79f6c5e
george-shammer <george.gbenle@outlook.com>
2022-03-26 13:14:33 +0000

    Create marketplace filter

--- src/components/Marketplace.js
+++ src/components/Marketplace.js
@@ -12,12 +12,12 @@ function Marketplace() {
                 <p className="market-text gold">Own, Rent, Borrow And Lend Unique Assets</p>
 
                 <div className="d-flex capsules">
-                    <input className="rounded-pill font-meridian search mt-3" type="text" placeholder="Search"></input>
+                    <input className="rounded-pill font-meridian gold search mt-3" type="text" placeholder="Search" />
                     <Link to="/WalletSetup" ><p className="wallet gold font-meridian rounded-pill mt-3 py-2 px-5">Sell</p></Link>
                     <Link to="/WalletSetup" ><p className="wallet gold font-meridian rounded-pill mt-3 py-2 px-5">Rent Out</p></Link>
 
 
-                    <select className="form-select rounded-pill px-5">
+                    <select className="form-select rounded-pill font-meridian gold search mt-3">
                         <option selected>Filter</option>
                         <option value="1">For Rent</option>
                         <option value="2">For Sale</option>

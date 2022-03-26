ccb9ad75244d604e99aab4415c2acddb4f2f0132
george-shammer <george.gbenle@outlook.com>
2022-03-26 11:33:22 +0000

    Update marketplace

--- src/components/Marketplace.js
+++ src/components/Marketplace.js
@@ -9,6 +9,10 @@ function Marketplace() {
             <div className="marketplace">
                 <p className="topic-marketplace font-medieval pt-5">Insignia Marketplace</p>
                 <p className="market-text gold">Own, Rent, Borrow And Lend Unique Assets</p>
+                <Link to="/WalletSetup" ><p className="wallet black font-meridian rounded-pill mt-3 py-2">Sell</p></Link>
+                {/* <div>
+                    <Link to="/WalletSetup" ><p className="wallet white font-meridian rounded-pill mt-3 py-2">Sell</p></Link>
+                </div> */}
             </div>
             
             <div className="py-3">

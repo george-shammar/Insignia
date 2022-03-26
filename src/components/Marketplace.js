8f2ddda50e22336a59f8f953404bdd3e8609e16f
george-shammer <george.gbenle@outlook.com>
2022-03-26 12:58:38 +0000

    Create marketplace filter

--- src/components/Marketplace.js
+++ src/components/Marketplace.js
@@ -17,7 +17,7 @@ function Marketplace() {
                     <Link to="/WalletSetup" ><p className="wallet gold font-meridian rounded-pill mt-3 py-2 px-5">Rent Out</p></Link>
 
 
-                    <select className="form-select">
+                    <select className="form-select rounded-pill px-5">
                         <option selected>Filter</option>
                         <option value="1">For Rent</option>
                         <option value="2">For Sale</option>

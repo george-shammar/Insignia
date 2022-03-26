7184089dfe303b5d6b9fa1df6488c5ddc361214e
george-shammer <george.gbenle@outlook.com>
2022-03-26 11:24:34 +0000

    Update marketplace

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from "../assets/Logo.png";
+import Marketplace from './Marketplace';
 import "../styles/Nav.css"
 
 function Nav() {
@@ -13,7 +14,7 @@ function Nav() {
         <div className="d-flex align-items-center font-meridian flex2">
           <p> <Link to="/" className="px-4 white">Home</Link></p>
           <p><a href="https://getinsignia.gitbook.io/whitepaper/" target="_blank" rel="noreferrer" className="px-4 white">Whitepaper</a></p>
-          {/* <p> <Link to="/MarketPLace" className="px-4 white">Market Place</Link></p> */}
+          <p> <Link to="/MarketPLace" className="px-4 white">Market Place</Link></p>
           {/* <p> <Link to="/News" className="px-4 white">News</Link></p> */}
         
         </div>

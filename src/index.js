b1664637faa569dc6d141abf7bcb172e3aeb87e4
george-shammar <george.gbenle@outlook.com>
2022-03-30 17:20:09 +0000

    Create wallet connection

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom';
 import Routes from './components/Routes';
 import "./styles/index.css"
 
-
 ReactDOM.render(
   <React.StrictMode>
     <Routes />
   </React.StrictMode>,
   document.getElementById('root')
 );
-

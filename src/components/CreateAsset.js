0bbcf5a24a3a92355e47598f90bd6bb9bb6840c5
george-shammar <george.gbenle@outlook.com>
2022-03-30 19:12:12 +0000

    Deploy smartcontract

--- src/components/CreateAsset.js
+++ src/components/CreateAsset.js
@@ -22,24 +22,24 @@ const CreateAsset = () => {
     const [formInput, updateFormInput] = useState({name:""});
     const [status, setStatus] = useState("");
 
-            useEffect(() => {
+      useEffect(() => {
               (async() => {
                 const {address} = await getCurrentWalletConnected();
                 setWallet(address)
             
                 addWalletListener();
               }) ()
-            }, []);
+      }, []);
 
-            // connect wallet 
-            const connectWalletPressed = async () => {
+      // connect wallet 
+      const connectWalletPressed = async () => {
               const walletResponse = await connectWallet();
               setWallet(walletResponse.address);
-          };
+      };
 
-          // wallet listener to update UI when wallet's state changes, 
-          // such as when the user disconnects or switches accounts.
-          function addWalletListener() {
+      // wallet listener to update UI when wallet's state changes, 
+      // such as when the user disconnects or switches accounts.
+      function addWalletListener() {
               if (window.ethereum) {
               window.ethereum.on("accountsChanged", (accounts) => {
                   if (accounts.length > 0) {
@@ -50,7 +50,9 @@ const CreateAsset = () => {
                   }
               });
               } 
-          }
+      }
+
+      
 
 
 

504564c70f5168114537b00c29128eeed8050a45
george-shammar <george.gbenle@outlook.com>
2022-03-30 21:13:14 +0000

    Connect frontend to contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,11 +3,54 @@ require("@nomiclabs/hardhat-waffle");
 // const infuraId = 
 
 module.exports = {
-  defaultNetwork: "hardhat",
+  // defaultNetwork: "testnet",
   networks: {
-    hardhat: {
-      chainId: 1337
+    localhost: {
+      url: "http://127.0.0.1:8545"
     },
+    hardhat: {
+    },
+    // testnet: {
+    //   url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+    //   chainId: 97,
+    //   gasPrice: 20000000000,
+    //   accounts: {mnemonic: ""}
+    // },
+    // mainnet: {
+    //   url: "https://bsc-dataseed.binance.org/",
+    //   chainId: 56,
+    //   gasPrice: 20000000000,
+    //   accounts: {mnemonic: mnemonic}
+    // }
+  },
+  solidity: {
+  version: "0.8.4",
+    settings: {
+      optimizer: {
+      enabled: true,
+      runs: 200
+    }
+    }
+  },
+  // paths: {
+  //   sources: "./contracts",
+  //   tests: "./test",
+  //   cache: "./cache",
+  //   artifacts: "./artifacts"
+  // },
+  // mocha: {
+  //   timeout: 20000
+  // }
+};
+
+
+
+// module.exports = {
+//   defaultNetwork: "hardhat",
+//   networks: {
+//     hardhat: {
+//       chainId: 1337
+//     },
     /*
     mumbai: {
       // Infura
@@ -22,15 +65,15 @@ module.exports = {
       accounts: [privateKey]
     }
     */
-  },
-  solidity: {
-    version: "0.8.4",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      }
-    }
-  }
-};
+//   },
+//   solidity: {
+//     version: "0.8.4",
+//     settings: {
+//       optimizer: {
+//         enabled: true,
+//         runs: 200
+//       }
+//     }
+//   }
+// };
 

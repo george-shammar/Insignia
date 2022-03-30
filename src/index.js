a3d3759ce0feb47e8971e3d38a477e1e43deef51
george-shammar <george.gbenle@outlook.com>
2022-03-30 17:19:14 +0000

    Create wallet connection

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './components/Routes';
-import { initContract } from './utils';
 import "./styles/index.css"
 
-window.nearInitPromise = initContract()
-  .then(() => {
+
 ReactDOM.render(
   <React.StrictMode>
     <Routes />
   </React.StrictMode>,
   document.getElementById('root')
 );
-})
-.catch(console.error)
+
